bae1a211fa8fb0c0047a7f8342386df66ec0d90a
afrazz <muhammedafras0@gmail.com>
2024-06-24 19:48:33 +0530

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,29 +3,37 @@
 @section('title', 'Home')
  
 @section('contents')
-<header class="bg-white shadow mb-4">
-    <div class="container mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold text-gray-900">
-            All profiles
-        </h1>
-    </div>
-</header>
+<head>
+<script src="https://kit.fontawesome.com/9f4528f967.js" crossorigin="anonymous"></script>
+</head>
+
+<h1 class="text-3xl font-bold text-gray-900 mb-4">
+    All profiles
+</h1>
 <main>
-    <div class="container mx-auto py-6 sm:px-6 lg:px-8">
-        <div class="row">
-            @foreach($otherUsersData as $userData)
-                <div class="col-md-3 mb-4">
-                    <div class="card h-100">
-                        <img src="{{ asset('storage/' . $userData->profile_pic) }}" class="card-img-top" alt="Profile Picture">
-                        <div class="card-body">
-                            <h5 class="card-title">Name: {{ $userData->fullname }}</h5>
-                            <p class="card-text">City: {{ $userData->city }}<br> Phone No: {{ $userData->mobile_number }}</p>
-                            <a href="{{ route('checkprofile', $userData->id) }}" class="btn btn-primary">Check Profile</a>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        @foreach($otherUsersData as $userData)
+            <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <img class="w-full" style="height: 225px; width:100%; object-fit:cover" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Outdoors-man-portrait_%28cropped%29.jpg/800px-Outdoors-man-portrait_%28cropped%29.jpg" alt="User Image">
+                <div class="px-3 py-3">
+                    <div class="font-bold text-xl mb-2">{{ $userData->fullname }}</div>
+                    <div class="flex justify-between flex-wrap">
+                        <div>
+                            <i class="fa-solid fa-location-dot"></i> {{ $userData->city }}
+                        </div>
+                        <div>
+                            <i class="fa-solid fa-phone"></i> {{ $userData->mobile_number }}
                         </div>
                     </div>
+                    <a href="{{ route('checkprofile', $userData->id) }}" class="mt-3 flex justify-center w-full items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" style="background-color: #D20062">
+                        Check Profile
+                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endforeach
     </div>
 </main>
 @endsection
--- resources/views/profiles/checkprofile.blade.php
+++ resources/views/profiles/checkprofile.blade.php
@@ -5,29 +5,42 @@
 @section('contents')
 <div class="container profile-card">
     <div class="row">
-        <div class="col-md-8 offset-md-2">
-            <div class="card">
-                <div class="card-header bg-primary text-white">
-                    <h1 class="h3 mb-0">Profile Details</h1>
+            <div class="card p-0 w-100">
+                <div class="card-header bg-primary text-white ">
+                    <h1 class="h3 mb-0 text-center">Profile Details</h1>
                 </div>
-                <div class="card-body">
-                    <p><strong>Full Name:</strong> {{ $userProfile->fullname }}</p>
-                    <p><strong>Gender:</strong> {{ $userProfile->gender }}</p>
-                    <p><strong>Date of Birth:</strong> {{ $userProfile->date_of_birth }}</p>
-                    <p><strong>Height:</strong> {{ $userProfile->height }}</p>
-                    <p><strong>Religion:</strong> {{ $userProfile->religion }}</p>
-                    <p><strong>City:</strong> {{ $userProfile->city }}</p>
-                    <p><strong>Mobile Number:</strong> {{ $userProfile->mobile_number }}</p>
-                    <p><strong>Education:</strong> {{ $userProfile->education }}</p>
-                    <p><strong>Occupation:</strong> {{ $userProfile->occupation }}</p>
-                    <p><strong>Annual Income:</strong> {{ $userProfile->annual_income }}</p>
+                <div class="flex justify-center mt-4">
                     @if ($userProfile->profile_pic)
-                        <p><strong>Profile Picture:</strong> <img src="{{ asset('storage/' . $userProfile->profile_pic) }}" alt="Profile Picture" class="img-thumbnail profile-picture"></p>
+                        <img src="{{ asset('storage/' . $userProfile->profile_pic) }}" style="height: 160px; width: 160px; border-radius: 50%" alt="Profile Picture" class="img-thumbnail profile-picture">
                     @endif
-                    
+                </div>
+                
+                <div class="card-body flex justify-center">
+                    <div>
+                        <ul class="list-group">
+                            <li class="list-group-item">Full Name: <strong>{{ $userProfile->fullname }}</strong></li>
+                            <li class="list-group-item">Gender: <strong>{{ $userProfile->gender }}</strong></li>
+                            <li class="list-group-item">Date of Birth: <strong>{{ $userProfile->date_of_birth }}</strong></li>
+                            <li class="list-group-item">Height: <strong>{{ $userProfile->height }}</strong></li>
+                            <li class="list-group-item">Religion: <strong>{{ $userProfile->religion }}</strong></li>
+                            <li class="list-group-item">City: <strong>{{ $userProfile->religion }}</strong></li>
+                        </ul>
+                    </div>
+
+                    <!-- <div>
+                        <p><strong>Full Name:</strong> {{ $userProfile->fullname }}</p>
+                        <p><strong>Gender:</strong> {{ $userProfile->gender }}</p>
+                        <p><strong>Date of Birth:</strong> {{ $userProfile->date_of_birth }}</p>
+                        <p><strong>Height:</strong> {{ $userProfile->height }}</p>
+                        <p><strong>Religion:</strong> {{ $userProfile->religion }}</p>
+                        <p><strong>City:</strong> {{ $userProfile->city }}</p>
+                        <p><strong>Mobile Number:</strong> {{ $userProfile->mobile_number }}</p>
+                        <p><strong>Education:</strong> {{ $userProfile->education }}</p>
+                        <p><strong>Occupation:</strong> {{ $userProfile->occupation }}</p>
+                        <p><strong>Annual Income:</strong> {{ $userProfile->annual_income }}</p>
+                    </div> -->
                 </div>
             </div>
-        </div>
     </div>
 </div>
 
